f2181ab6ab6158b4139c32114ada036821005a08
Jean Paul Vale da Silva <jean.silva@niduu.com>
2022-07-05 19:25:53 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <p>Jean Paul</p>
+        <p>Jean Paul 2</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
